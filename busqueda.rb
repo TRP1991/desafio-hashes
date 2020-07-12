75b334492393767d44b5759b93b609b26cd896e5
Tamara Rubilar <ta.rubilar@gmail.com>
2020-07-12 01:51:29 -0400

    programs 1, 2 and 3 executed. In process program 4

---
+++ busqueda.rb
@@ -0,0 +1,45 @@
+=begin
+
+Desafío 4
+
+Crear un programa llamado busqueda.rb que pueda buscar a que mes pertenece una o mas cifras
+específica. En caso de no encontrarlo mostrar el mensaje "no encontrado"
+
+Uso:
+ruby busqueda.rb 15000 31000 27000
+
+    Enero no encontrado no encontrado
+
+Se asumirá que en ningún mes la venta se repite.
+
+Tips:
+
+    Se evalúa la salida en pantalla por lo que se debe respetar los mensajes.
+    Se evaluará con un hash distinto al mostrado.
+
+=end    
+
+ventas = {
+    Enero: 15000,
+    Febrero: 22000,
+    Marzo: 12000,
+    Abril: 17000,
+    Mayo: 81000,
+    Junio: 13000,
+    Julio: 21000,
+    Agosto: 41200,
+    Septiembre: 25000,
+    Octubre: 21500,
+    Noviembre: 91000,
+    Diciembre: 21000
+}
+
+match = '15000'
+ventas.each do |k,v|
+    if v == match
+        founded = true
+        puts k
+    end
+end
+
+puts 'no encontrado' unless founded
---
+++ iter3.rb
@@ -0,0 +1,47 @@
+=begin
+
+Desafío 3
+
+Crear un programa llamado iter3.rb que tenga un método llamado filter que reciba un hash y
+devuelva un hash nuevo con los valores superior a un parámetro que será ingresado al momento de
+llamar al programa.
+
+Uso:
+
+filter(45000)
+
+    No hay output, se evaluará el retorno del método
+
+Tips:
+
+Respetar el nombre del programa.
+Respetar el nombre del método.
+Se evaluará el llamado al método
+El hash utilizado para evaluar puede ser distinto y tener mas o menos meses.
+
+=end 
+
+def filter(hash)
+    filtered_hash = {}
+    hash.each do |k,v|
+        if v > 45000
+            filtered_hash[k] = v
+        end
+    end
+    filtered_hash #return
+end
+
+filter ({
+    Enero: 15000,
+    Febrero: 22000,
+    Marzo: 12000,
+    Abril: 17000,
+    Mayo: 81000,
+    Junio: 13000,
+    Julio: 21000,
+    Agosto: 41200,
+    Septiembre: 25000,
+    Octubre: 21500,
+    Noviembre: 91000,
+    Diciembre: 21000
+})
